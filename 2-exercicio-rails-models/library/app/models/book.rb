4fd5916e36c2f85a4cfe3c99141def81e2092f9c
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2014-03-27 21:10:52 -0300

    Adiciona escopos

--- 2-exercicio-rails-models/library/app/models/book.rb
+++ 2-exercicio-rails-models/library/app/models/book.rb
@@ -1,5 +1,9 @@
 class Book < ActiveRecord::Base
   belongs_to :user
   validates :name, presence: true
-  
+
+  scope :by_name, lambda { where(name: "1984") }
+  scope :after, lambda { |time| where("created_at > ?", time) }
+  scope :most_recent, lambda { order("created_at DESC").limit(5) }
+  scope :most_recent_by_user, lambda { |id| where(user_id: id).most_recent }
 end
