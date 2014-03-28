748564f3fde166b34159c16dd2955b091700b693
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2014-03-27 21:17:26 -0300

    Corrige identação do arquivo

--- 2-exercicio-rails-models/library/app/models/user.rb
+++ 2-exercicio-rails-models/library/app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
-	validates :first_name, presence: true
-	validates :email, presence: true
-	has_many :books 
-	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
-	has_many :books
+  # nome_completo
+
+  validates :first_name, presence: true
+  validates :email, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  has_many :books
 
   before_create :set_complete_name
   after_destroy :say_goodbye
