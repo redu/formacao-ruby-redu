3e4728fa2d8b91d71ba51a0bfca7eebedebdd7c4
embs <embs@cin.ufpe.br>
2015-03-15 00:52:58 -0300

    Seta e utiliza subject nos testes em string_set_spec

--- 1-exercicio-ruby/string_set_spec.rb
+++ 1-exercicio-ruby/string_set_spec.rb
@@ -1,24 +1,25 @@
 require_relative 'string_set'
 
 describe StringSet do
+  subject { StringSet.new items }
+
   let(:items) { ["Ruby", "Python", "Java", "Perl"] }
-  let(:string_set) { StringSet.new items }
   let(:another_string_set) { StringSet.new(items << "Dart") }
 
   describe '#initialize' do
 
     it 'returns StringSet object' do
-      expect(string_set.class).to eq StringSet
+      expect(subject.class).to eq StringSet
     end
 
     it 'initializes StringSet items' do
-      expect(string_set.items).to eq items
+      expect(subject.items).to eq items
     end
   end
 
   describe '#add' do
     let(:new_item) { "Javascript" }
-    let(:new_string_set) { string_set.add(new_item) }
+    let(:new_string_set) { subject.add(new_item) }
 
     xit 'returns new StringSet object' do
       expect(new_string_set.class).to eq StringSet
@@ -32,35 +33,35 @@ describe StringSet do
   describe '#exists?' do
 
     it 'returns false for nonexistent item' do
-      expect(string_set.exists?("nonexistent")).to eq false
+      expect(subject.exists?("nonexistent")).to eq false
     end
 
     it 'returns true for existent item' do
-      expect(string_set.exists?("Ruby")).to eq true
+      expect(subject.exists?("Ruby")).to eq true
     end
   end
 
   describe '#union' do
-    let(:union_string_set) { string_set.union another_string_set }
+    let(:union_string_set) { subject.union another_string_set }
 
     it 'returns new StringSet object' do
-      expect(union_string_set.equal?(string_set)).to eq false
+      expect(union_string_set.equal?(subject)).to eq false
     end
 
     it 'returns performs union' do
-      expect(union_string_set.items).to eq string_set.items | another_string_set.items
+      expect(union_string_set.items).to eq subject.items | another_string_set.items
     end
   end
 
   describe '#union!' do
-    let(:union_string_set) { string_set.union! another_string_set }
+    let(:union_string_set) { subject.union! another_string_set }
 
     it 'returns same StringSet object' do
-      expect(union_string_set.equal?(string_set)).to eq true
+      expect(union_string_set.equal?(subject)).to eq true
     end
 
     it 'returns performs union' do
-      expect(union_string_set.items).to eq string_set.items | another_string_set.items
+      expect(union_string_set.items).to eq subject.items | another_string_set.items
     end
   end
 end
